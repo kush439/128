c413330968943a9e4f5cc42d40f3fd0c32bd3b9c
Kush D. Patel <80115450+kush439@users.noreply.github.com>
2021-12-04 11:54:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+song = "";
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+song = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    canvas.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+} 
+
+function gotPoses(results){
+ if(results.length>0)
+ {
+     console.log(results);
+     leftWristX = results[0].pose.leftWrist.x;
+     leftWristY = results[0].pose.rightWrist.y;
+     console.log("leftWristX = " +  leftWristX + "leftWristY = " +  leftWristY );
+
+     rightWristX = results[0].pose.rightWrist.x;
+     rightWristY = results[0].pose.rightWrist.y;
+     console.log("rightWristX = " +  rightWristX + "rightWristY = " +  rightWristY );
+ }
+}
+
+function modelLoaded(){
+    console.log('Posenet is Initialised');
+function play()
+}
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(5);
+}
+
+function draw()
+{
+    image(video, 0 , 0 , 600 , 500);
+}
